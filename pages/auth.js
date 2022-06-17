f20e319d31361ba72546ac38bc46a0f32c540729
Omar Maroki <omar987@hotmail.co.uk>
2022-06-17 19:41:49 +0100

    .

--- pages/auth.js
+++ pages/auth.js
@@ -1,108 +1,12 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React from 'react'
+import AuthModal from '../components/authModal'
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+const auth = () => {
+	return (
+		<>
+			<AuthModal />
+		</>
+	)
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="LOGIN" {...a11yProps(0)} />
-          <Tab label="SIGNUP" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-    </Box>
-  );
-}
-
-// import React, { useState } from "react";
-// import Box from "@mui/material/Box";
-// import Tabs from "@mui/material/Tabs";
-// import Tab from "@mui/material/Tab";
-
-// const auth = () => {
-//   const [value, setValue] = useState(0);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-
-//     return (
-//       <div>
-//         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-//           <Tabs
-//             value={value}
-//             onChange={handleChange}
-//             aria-label="basic tabs example"
-//           >
-//             <Tab label="Item One" {...a11yProps(0)} />
-//             <Tab label="Item Two" {...a11yProps(1)} />
-//             <Tab label="Item Three" {...a11yProps(2)} />
-//           </Tabs>
-//         </Box>
-//         <TabPanel value={value} index={0}>
-//           Item One
-//         </TabPanel>
-//         <TabPanel value={value} index={1}>
-//           Item Two
-//         </TabPanel>
-//         <TabPanel value={value} index={2}>
-//           Item Three
-//         </TabPanel>
-//       </div>
-//     );
-//   };
-// };
-
-// export default auth;
+export default auth
