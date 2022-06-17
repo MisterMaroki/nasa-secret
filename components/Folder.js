30dfea949b2734e0e5b68d90f3627350fc04d5f2
Omar Maroki <omar987@hotmail.co.uk>
2022-06-18 00:44:51 +0100

    styled folder component

--- components/Folder.js
+++ components/Folder.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FolderIcon from '@mui/icons-material/Folder'
 import Image from 'next/image'
 const Folder = () => {
+	const [selected, setSelected] = useState(false)
+
+	const select = (e) => {
+		e.stopPropagation()
+		setSelected(true)
+	}
+	window.addEventListener('click', (e) => {
+		// console.log(e.target.toString())
+		setSelected(false)
+	})
 	return (
-		<div className="folder-container">
-			{/* <FolderIcon size="large" sx={{ fontSize: "6rem" }} /> */}
-			<Image src={'/folder.svg'} layout="fill" alt="folder" />
+		<div
+			className={`${selected && 'active'} folder-container`}
+			onClick={select}
+			key="5"
+		>
+			<div
+				style={{
+					padding: '0 7px',
+					borderRadius: '7px',
+					backgroundColor: selected ? 'rgb(57, 56, 55)' : 'transparent',
+				}}
+			>
+				<div className="img-container">
+					<Image
+						src={'/folder.svg'}
+						layout="fill"
+						alt="folder"
+						style={{ maxWidth: '10px' }}
+					/>
+				</div>
+			</div>
+			<h3>NASA</h3>
 		</div>
 	)
 }
--- pages/auth.js
+++ pages/auth.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import AuthModal from "../components/auth/authModal";
-import { AuthState } from "../AuthContext";
-import styles from "../styles/Home.module.scss";
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import AuthModal from '../components/auth/authModal'
+import { AuthState } from '../AuthContext'
+import styles from '../styles/Home.module.scss'
 
 const auth = () => {
-  const router = useRouter();
-  const { user } = AuthState();
+	const router = useRouter()
+	const { user } = AuthState()
 
-  useEffect(() => {
-    if (user) {
-      router.push("/");
-    }
-  }, [user]);
+	useEffect(() => {
+		if (user) {
+			router.push('/')
+		}
+	}, [user])
 
-  return (
-    <div className={[styles.container, styles.main].join(" ")}>
-      <AuthModal />
-    </div>
-  );
-};
+	return (
+		<div className={[styles.container, styles.main].join(' ')}>
+			<AuthModal />
+		</div>
+	)
+}
 
-export default auth;
+export default auth
 
 //https://api.nasa.gov/planetary/apod
 //
