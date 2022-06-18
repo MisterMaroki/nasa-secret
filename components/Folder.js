6b80b5b18b9a9fc528726d8ceece7548e7fee2a4
Omar Maroki <omar987@hotmail.co.uk>
2022-06-18 01:27:23 +0100

    selections exclusive now

--- components/Folder.js
+++ components/Folder.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react'
 import FolderIcon from '@mui/icons-material/Folder'
 import Image from 'next/image'
-const Folder = () => {
-	const [selected, setSelected] = useState(false)
-
-	const select = (e) => {
-		e.stopPropagation()
-		setSelected(true)
-	}
-	window.addEventListener('click', (e) => {
-		// console.log(e.target.toString())
-		setSelected(false)
-	})
+const Folder = ({ select, selected, title, route }) => {
 	return (
 		<div
-			className={`${selected && 'active'} folder-container`}
-			onClick={select}
+			className={`${selected === title && 'active'} folder-container`}
+			onClick={(e) => select(e, title)}
 			key="5"
 		>
 			<div
 				style={{
 					padding: '4px 7px',
 					borderRadius: '7px',
-					backgroundColor: selected ? 'rgb(57, 56, 55)' : 'transparent',
+					backgroundColor:
+						selected === title ? 'rgb(57, 56, 55)' : 'transparent',
 				}}
 			>
 				<div className="img-container">
@@ -34,7 +25,7 @@ const Folder = () => {
 					/>
 				</div>
 			</div>
-			<h3>NASA</h3>
+			<h3>{title}</h3>
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { AuthState } from '../AuthContext'
 import Folder from '../components/Folder'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Auth.module.scss'
 
 const Home = () => {
 	const router = useRouter()
 	const { user } = AuthState()
 
+	const [selected, setSelected] = useState(null)
+
+	const select = (e, folderName) => {
+		e.stopPropagation()
+		setSelected(folderName)
+	}
+
 	useEffect(() => {
 		if (user === null) {
 			router.push('/auth')
 		}
+		const handleClick = (e) => {
+			// console.log(e.target.toString())
+			setSelected(null)
+		}
+		window.addEventListener('click', handleClick)
 	}, [router, user])
 
 	return (
 		user && (
 			<div className={styles.container}>
-				<Folder />
+				<Folder
+					select={select}
+					selected={selected}
+					title="NASA"
+					route="/folder/nasa"
+				/>
+				<Folder
+					select={select}
+					selected={selected}
+					title="SpaceX"
+					route="/folder/spacex"
+				/>
 			</div>
 		)
 	)
