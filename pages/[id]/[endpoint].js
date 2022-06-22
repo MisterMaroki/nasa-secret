d4b489a529c585efd35cf71b7822cda1a20bf161
Omar Maroki <omar987@hotmail.co.uk>
2022-06-22 16:38:06 +0100

    prep for building more route logic

--- pages/[id]/[endpoint].js
+++ pages/[id]/[endpoint].js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const Endpoint = ({ endpoint }) => {
-	return <div>{endpoint}</div>
+const Endpoint = ({ endpoint, params }) => {
+	return (
+		<div>
+			{endpoint}
+			{params.id}
+			{params.endpoint}
+		</div>
+	)
 }
 
 export default Endpoint
 
 export async function getServerSideProps(context) {
 	return {
-		props: { endpoint: context.resolvedUrl }, // will be passed to the page component as props
+		props: { endpoint: context.resolvedUrl, params: context.params }, // will be passed to the page component as props
 	}
 }
