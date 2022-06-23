a7e7c56876f95d0615a0035cef24356e9b7a5162
Omar Maroki <omar987@hotmail.co.uk>
2022-06-23 22:44:05 +0100

    fix draggable blocking click on mobile

--- components/Folder.js
+++ components/Folder.js
@@ -3,6 +3,7 @@ import FolderIcon from '@mui/icons-material/Folder'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Draggable from 'react-draggable'
+import { isMobile } from 'react-device-detect'
 
 const Folder = ({ select, selected, title, route, index }) => {
 	const router = useRouter()
@@ -14,34 +15,55 @@ const Folder = ({ select, selected, title, route, index }) => {
 	}
 
 	return (
-		<Draggable nodeRef={nodeRef}>
-			<div
-				ref={nodeRef}
-				className={`${selected === index && 'active'} folder-container`}
-				onClick={handleClick}
-				key="5"
-			>
-				<div
-					style={{
-						padding: '10px ',
-						borderRadius: '7px',
-						backgroundColor:
-							selected === index ? 'rgb(57, 56, 55)' : 'transparent',
-					}}
-				>
-					<div className="img-container">
-						<Image
-							src={'/folder.svg'}
-							layout="fill"
-							alt="folder"
-							draggable="false"
-						/>
-					</div>
-				</div>
-				<h3>{title}</h3>
-			</div>
-		</Draggable>
+		<>
+			{!isMobile ? (
+				<Draggable nodeRef={nodeRef}>
+					<Element
+						title={title}
+						nodeRef={nodeRef}
+						selected={selected}
+						index={index}
+						handleClick={handleClick}
+					/>
+				</Draggable>
+			) : (
+				<Element
+					title={title}
+					nodeRef={nodeRef}
+					selected={selected}
+					index={index}
+					handleClick={handleClick}
+				/>
+			)}
+		</>
 	)
 }
 
 export default Folder
+
+const Element = ({ title, nodeRef, selected, index, handleClick }) => (
+	<div
+		ref={nodeRef}
+		className={`${selected === index && 'active'} folder-container`}
+		onClick={handleClick}
+		key="5"
+	>
+		<div
+			style={{
+				padding: '10px ',
+				borderRadius: '7px',
+				backgroundColor: selected === index ? 'rgb(57, 56, 55)' : 'transparent',
+			}}
+		>
+			<div className="img-container">
+				<Image
+					src={'/folder.svg'}
+					layout="fill"
+					alt="folder"
+					draggable="false"
+				/>
+			</div>
+		</div>
+		<h3>{title}</h3>
+	</div>
+)
